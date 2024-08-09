a48a6c63094c132772177c65b613be906611e0f5
WaichL <waiching.lin@gmail.com>
2024-08-09 12:54:08 +0800

    updated imapact responsiveness

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,27 +1,56 @@
-let currentValue = 0;
-let electricityValue = 2305;
-let electricityEqvValue = 2 * electricityValue;
-let electricityEqvUnit = "Tons of carbon saved";
+
+let elec = 41000
+let co2e = 270
+let trees = 6750
+let cars = 70
+let dollar = 0.6 
+
+let elecUnit = "KWh Saved"
+let co2eUnit  = "CO2e Reduced"
+let treesUnit = "Trees Absorbed Carbon"
+let carsUnit = "Times of Driving Round the Earth"
+let dollarUnit = "Millions of HKD saved"
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('electricity').innerText = electricityValue;
-  document.getElementById('electricity-eqv').innerText = electricityEqvValue;
-  document.getElementById('electricity-eqv-unit').innerText = electricityEqvUnit;
+  document.getElementById('electricity').innerText = elec;
+  document.getElementById('electricity-unit').innerText = elecUnit;
+  document.getElementById('electricity-eqv').innerText = co2e;
+  document.getElementById('electricity-eqv-unit').innerText = co2eUnit;
 });
 
-function updateValue(amount) {
-  currentValue += amount;
-  document.getElementById('number-value').innerText = currentValue;
+// function updateValue(amount) {
+//   currentValue += amount;
+//   document.getElementById('number-value').innerText = currentValue;
+// }\
+
+// function resetValue() {
+//   currentValue = 0;
+//   document.getElementById('number-value').innerText = currentValue;
+// }
+
+// function updateEqvValue() {
+//   electricityEqvValue = 10;
+//   electricityEqvUnit = "trees planted";
+//   document.getElementById('electricity-eqv').innerText = electricityEqvValue;
+//   document.getElementById('electricity-eqv-unit').innerText = electricityEqvUnit;
+// }
+
+function updateToTrees() {
+  document.getElementById('electricity-eqv').innerText = trees;
+  document.getElementById('electricity-eqv-unit').innerText = treesUnit;
 }
 
-function resetValue() {
-  currentValue = 0;
-  document.getElementById('number-value').innerText = currentValue;
+function updateToCo2e() {
+  document.getElementById('electricity-eqv').innerText = co2e;
+  document.getElementById('electricity-eqv-unit').innerText = co2eUnit;
 }
 
-function updateEqvValue() {
-  electricityEqvValue = 10;
-  electricityEqvUnit = "trees planted";
-  document.getElementById('electricity-eqv').innerText = electricityEqvValue;
-  document.getElementById('electricity-eqv-unit').innerText = electricityEqvUnit;
+function updateToCars() {
+  document.getElementById('electricity-eqv').innerText = cars;
+  document.getElementById('electricity-eqv-unit').innerText = carsUnit;
+}
+
+function updateToDollar() {
+  document.getElementById('electricity-eqv').innerText = dollar;
+  document.getElementById('electricity-eqv-unit').innerText = dollarUnit;
 }
